9b464e6e9152cde30debe2beb0b159e2edbdfa37
Anastasiia <114518969+AnaSokolova05@users.noreply.github.com>
2024-04-30 23:15:06 +0100

    Delete lab2/src directory

--- lab2/src/Counter.java
+++
@@ -1,7 +0,0 @@
-
-public interface Counter 
-{
-	int countWords(String sentence); // Counting words
-	int countLetters(String sentence); //Counting letters
-	int getLength(String sentence); // Counting length 
-}
