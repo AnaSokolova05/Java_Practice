4b6f3625a361b665046159c8114345bd42a7b816
Anastasiia <114518969+AnaSokolova05@users.noreply.github.com>
2024-04-30 23:15:56 +0100

    Add files via upload

---
+++ lab2/src/Counter.java
@@ -0,0 +1,7 @@
+
+public interface Counter 
+{
+	int countWords(String sentence); // Counting words
+	int countLetters(String sentence); //Counting letters
+	int getLength(String sentence); // Counting length 
+}
