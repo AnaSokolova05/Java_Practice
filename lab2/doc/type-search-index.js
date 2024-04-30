7db89ea6e2ffd39d40ae5246d872017afbd7797f
Anastasiia <114518969+AnaSokolova05@users.noreply.github.com>
2024-04-30 23:09:15 +0100

    Delete lab2/doc directory

--- lab2/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Counter"},{"p":"<Unnamed>","l":"Driver"},{"p":"<Unnamed>","l":"WordProcessor"}];updateSearchResults();
